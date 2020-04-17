f3270bb18282bf5039fa2bbe19052e1aa64d9940
Koushik <kmandalforstudy@gmail.com>
2020-04-17 13:17:26 +0200

    adding Xsec

--- FileList_2016.py
+++ FileList_2016.py
@@ -7,12 +7,24 @@ Data_dir = MC_dir
 Signal_dir = "StopSignal/"
 
 samples = {}
-#need to add more options like Xsec, Nevents etc.
-samples['Stop_500_480_fast'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/tau10mm/FastSim/"), 1.00]
-samples['Stop_500_480_full'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/tau10mm/FullSim/"), 1.00] 
+#samples[samplename] = [path, Xsec, Nevents].
+#Xec twiki
+#https://twiki.cern.ch/twiki/bin/view/LHCPhysics/SUSYCrossSections13TeVstopsbottom#NNLOapprox_NNLL
+#https://twiki.cern.ch/twiki/bin/viewauth/CMS/SummaryTable1G25ns
+#From Vienna AN framework
+#https://github.com/HephyAnalysisSW/Samples/blob/master/nanoAOD/python/Summer16_nanoAODv6.py
 
-samples['TTSingleLep_pow'] = [os.path.join(userpath, MC_dir, "TTSingleLep_pow/"), 1.00]
-samples['TTLep_pow'] = [os.path.join(userpath, MC_dir, "TTLep_pow/"), 1.00]
+samples['Stop_500_480_prompt_fast'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/prompt/FastSim/"), 0.609, ]
+samples['Stop_500_480_prompt_full'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/prompt/FullSim/"), 0.609, ]
+samples['Stop_500_480_tau10mm_fast'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/tau10mm/FastSim/"), 0.609, 339618]
+samples['Stop_500_480_tau10mm_full'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/tau10mm/FullSim/"), 0.609, 377544]
+samples['Stop_500_480_tau100mm_fast'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/tau100mm/FastSim/"), 0.609, 219739]
+samples['Stop_500_480_tau100mm_full'] = [os.path.join(userpath, Signal_dir, "Stop_500_480/tau100mm/FullSim/"), 0.609, 398443] 
+
+
+
+samples['TTSingleLep_pow'] = [os.path.join(userpath, MC_dir, "TTSingleLep_pow/"), 831.762*(3*0.108)*(1-3*0.108)*2, 2678436]
+samples['TTLep_pow'] = [os.path.join(userpath, MC_dir, "TTLep_pow/"), 831.762*((3*0.108)**2), 1.00]
 samples['WJetsToLNu_HT70to100'] = [os.path.join(userpath, MC_dir, "WJetsToLNu_HT70to100/"), 1.00]
 samples['WJetsToLNu_HT100to200'] = [os.path.join(userpath, MC_dir, "WJetsToLNu_HT100to200/"), 1.00]
 samples['WJetsToLNu_HT200to400'] = [os.path.join(userpath, MC_dir, "WJetsToLNu_HT200to400/"), 1.00]
