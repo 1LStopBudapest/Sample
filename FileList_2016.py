b673309297ba930ed3f053d09a15f87e42d725ed
Armin Kadlecsik <wellington2277@gmail.com>
2022-03-09 15:33:24 +0100

    Added T2tt signal samples

--- FileList_2016.py
+++ FileList_2016.py
@@ -12,6 +12,9 @@ StopSignal_500_480_dir = "StopSignal/Stop_500_480/"
 StopSignal_500_485_dir = "StopSignal/Stop_500_485/"
 StopSignal_500_490_dir = "StopSignal/Stop_500_490/"
 V9_nanoAOD_500_485_dir = "StopSignal/V9/Stop_500_485/"
+UL16PostVFPStopSignal_dir = "PostProcessedNtuple/2016/UL/PostVFP/FullsimSig/"
+UL16PreVFPStopSignal_dir = "PostProcessedNtuple/2016/UL/PreVFP/FullsimSig/"
+
 BKdir = "TestFile/"
 
 samples = {}
@@ -372,3 +375,22 @@ samples['UL16PostVFP_MET_Data'] = [samples['UL16PostVFP_MET_Run2016F'], samples[
 
 # TTGJets?
 
+
+
+samples['UL16PostVFP_Sig_Displaced_300_290'] = [os.path.join(userpath, UL16PostVFPStopSignal_dir, "Displaced/SMS_T2tt_LL_mStop_300_mLSP_290/"), ]
+samples['UL16PostVFP_Sig_Displaced_350_335'] = [os.path.join(userpath, UL16PostVFPStopSignal_dir, "Displaced/SMS_T2tt_LL_mStop_350_mLSP_335/"), ]
+samples['UL16PostVFP_Sig_Displaced_400_380'] = [os.path.join(userpath, UL16PostVFPStopSignal_dir, "Displaced/SMS_T2tt_LL_mStop_400_mLSP_380/"), ]
+samples['UL16PostVFP_Sig_Displaced'] = [samples['UL16PostVFP_Sig_Displaced_300_290'], samples['UL16PostVFP_Sig_Displaced_350_335'], samples['UL16PostVFP_Sig_Displaced_400_380']]
+
+samples['UL16PostVFP_Sig_Prompt_500_420'] = [os.path.join(userpath, UL16PostVFPStopSignal_dir, "Prompt/SMS_T2tt_LL_mStop_500_mLSP_420/"), ]
+samples['UL16PostVFP_Sig_Prompt_500_450'] = [os.path.join(userpath, UL16PostVFPStopSignal_dir, "Prompt/SMS_T2tt_LL_mStop_500_mLSP_450/"), ]
+samples['UL16PostVFP_Sig_Prompt_500_470'] = [os.path.join(userpath, UL16PostVFPStopSignal_dir, "Prompt/SMS_T2tt_LL_mStop_500_mLSP_470/"), ]
+
+
+samples['UL16PreVFP_Sig_Displaced_300_290'] = [os.path.join(userpath, UL16PreVFPStopSignal_dir, "Displaced/SMS_T2tt_LL_mStop_300_mLSP_290/"), ]
+samples['UL16PreVFP_Sig_Displaced_350_335'] = [os.path.join(userpath, UL16PreVFPStopSignal_dir, "Displaced/SMS_T2tt_LL_mStop_350_mLSP_335/"), ]
+samples['UL16PreVFP_Sig_Displaced_400_380'] = [os.path.join(userpath, UL16PreVFPStopSignal_dir, "Displaced/SMS_T2tt_LL_mStop_400_mLSP_380/"), ]
+
+samples['UL16PreVFP_Sig_Prompt_500_420'] = [os.path.join(userpath, UL16PreVFPStopSignal_dir, "Prompt/SMS_T2tt_LL_mStop_500_mLSP_420/"), ]
+samples['UL16PreVFP_Sig_Prompt_500_450'] = [os.path.join(userpath, UL16PreVFPStopSignal_dir, "Prompt/SMS_T2tt_LL_mStop_500_mLSP_450/"), ]
+samples['UL16PreVFP_Sig_Prompt_500_470'] = [os.path.join(userpath, UL16PreVFPStopSignal_dir, "Prompt/SMS_T2tt_LL_mStop_500_mLSP_470/"), ]
