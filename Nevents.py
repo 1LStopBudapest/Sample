733a3c6dca7f33df4e12c19b1bb6fe182714dc8d
Koushik <kmandalforstudy@gmail.com>
2022-05-29 15:41:05 +0200

    small change in Nevents.py

--- Nevents.py
+++ Nevents.py
@@ -8,13 +8,16 @@ def get_parser():
     '''
     import argparse
     argParser = argparse.ArgumentParser(description = "Argument parser")
-    argParser.add_argument('--sample',           action='store',                     type=str,            default='Stop_500_480_prompt_fast',                                help="Which sample?" )
-    
+    argParser.add_argument('--sample',           action='store',                     type=str,            default='TTLep_pow',                                help="Which sample?" )
+    argParser.add_argument('--year',             action='store',                     type=str,            default='2016PostVFP',                                             help="Which year?" )
+    argParser.add_argument('--proc',             action='store',                     type=str,            default='other',                                             help="Which year?" )    
     return argParser
 
 options = get_parser().parse_args()
 
 sample  = options.sample
+year = options.year
+proc = options.proc
 
-ch = SampleChain(sample, 0, -1).getchain()
+ch = SampleChain(sample, 0, -1, year, proc).getchain()
 print 'Total Events for ', sample, ': ', ch.GetEntries()
